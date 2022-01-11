8e98138ba9b74d51e9c3ab6057ae7b7b6054ab8f
phantomhaze <ahern077@gmail.com>
2022-01-11 12:09:46 -0800

    added about section

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header class="text-gray-400 bg-gray-900 body-font">
+      <header class="text-slate-400 bg-slate-900 body-font">
         <div class="container mx-auto flex flex-wrap flex-col md:flex-row items-center">
           <a class="flex title-font font-medium items-center text-black mb-4 md:mb-0">
           <img src='/inkpx-word-art.png' class="h-16"/>
@@ -19,7 +19,7 @@ export default function Home() {
             <a class="mr-5 hover:text-black">Contract</a>
             <a class="mr-5 hover:text-black">OpenSea</a>
           </nav>
-          <button class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-red-700 border-2 border-red-700 mt-4 md:mt-0">Connect
+          <button class="inline-flex items-center bg-slate-800 border-0 py-1 px-3 focus:outline-none hover:bg-slate-700 rounded text-red-700 border-2 border-red-700 mt-4 md:mt-0">Connect
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
@@ -36,20 +36,37 @@ export default function Home() {
               <img class="object-cover object-center rounded" alt="hero" src="pixeldiceavie.png"/>
             </div>
             <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-              <h1 class="title-font sm:text-6xl text-4xl mb-4 font-medium text-black opacity-90 tracking-wider z-10">Every Pixel 
+              <h1 class="title-font sm:text-6xl text-4xl mb-4 font-medium text-black opacity-90 tracking-wider z-10">Every Dice 
                 <br class="hidden lg:inline-block"/> is unique  
               </h1>
               {/* <p class="mb-8 leading-relaxed opacity-70 text-xl">These furry friends are looking for a cuddle and a new home</p> */}
               <div class="flex justify-center">
-                <button class="inline-flex text-red-500 bg-white border-0 py-2 px-6 focus:outline-none hover:bg-red-600 hover:text-white rounded text-xl font-bold tracking-wider">Mint</button>
-                <button class="ml-4 inline-flex text-white bg-red-700 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">OpenSea</button>
+              <button class="inline-flex text-red-500 bg-white border-0 py-2 px-6 focus:outline-none hover:bg-red-600 hover:text-white rounded text-xl font-bold tracking-wider">OpenSea</button>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* About */}
+        <section class="text-slate-400 bg-slate-900 body-font">
+          <div class="container px-5 py-24 mx-auto flex flex-wrap">
+            <h2 class="sm:text-3xl text-2xl text-white font-medium title-font mb-2 md:w-2/5">What are Pixel Dice?</h2>
+            <div class="md:w-3/5 md:pl-6">
+              <p class="leading-relaxed text-4xl">Pixel Dice is a collection of 10000 pixelated dice that live on in the blockchain, each one unique!</p>
+              <div class="flex md:mt-4 mt-6">
+              <button class="ml-4 inline-flex text-white bg-red-700 border-0 py-2 px-6 focus:outline-none hover:bg-slate-700 hover:text-white rounded text-lg">OpenSea</button>
+                <a class="text-indigo-400 inline-flex items-center ml-4">Join our Discord 
+                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                    <path d="M5 12h14M12 5l7 7-7 7"></path>
+                  </svg>
+                </a>
               </div>
             </div>
           </div>
         </section>
 
         {/* team members */}
-        <section class="text-gray-400 bg-gray-900 body-font">
+        <section class="text-slate-400 bg-slate-900 body-font">
           <div class="container px-5 py-24 mx-auto">
             <div class="flex flex-col text-center w-full mb-20">
               <h1 class="text-2xl font-medium title-font mb-4 text-white">OUR TEAM</h1>
@@ -61,20 +78,20 @@ export default function Home() {
                   <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/200x200"/>
                   <div class="w-full">
                     <h2 class="title-font font-medium text-lg text-white">Arm Production</h2>
-                    <h3 class="text-gray-500 mb-3">CEO</h3>
+                    <h3 class="text-slate-500 mb-3">CEO</h3>
                     <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                     <span class="inline-flex">
-                      <a class="text-gray-700">
+                      <a class="text-slate-700">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" class="w-5 h-5" viewBox="0 0 24 24">
                           <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                         </svg>
                       </a>
-                      <a class="ml-2 text-gray-700">
+                      <a class="ml-2 text-slate-700">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" class="w-5 h-5" viewBox="0 0 24 24">
                           <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                         </svg>
                       </a>
-                      <a class="ml-2 text-gray-700">
+                      <a class="ml-2 text-slate-700">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" class="w-5 h-5" viewBox="0 0 24 24">
                           <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                         </svg>
@@ -88,20 +105,20 @@ export default function Home() {
                   <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/201x201"/>
                   <div class="w-full">
                     <h2 class="title-font font-medium text-lg text-white">CrypticAlex</h2>
-                    <h3 class="text-gray-500 mb-3">Server Manager</h3>
+                    <h3 class="text-slate-500 mb-3">Server Manager</h3>
                     <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                     <span class="inline-flex">
-                      <a class="text-gray-700">
+                      <a class="text-slate-700">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" class="w-5 h-5" viewBox="0 0 24 24">
                           <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                         </svg>
                       </a>
-                      <a class="ml-2 text-gray-700">
+                      <a class="ml-2 text-slate-700">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" class="w-5 h-5" viewBox="0 0 24 24">
                           <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                         </svg>
                       </a>
-                      <a class="ml-2 text-gray-700">
+                      <a class="ml-2 text-slate-700">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" class="w-5 h-5" viewBox="0 0 24 24">
                           <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                         </svg>
@@ -115,20 +132,20 @@ export default function Home() {
                   <img alt="team" class="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4" src="https://dummyimage.com/202x202"/>
                   <div class="w-full">
                     <h2 class="title-font font-medium text-lg text-white">SpinSpinFrog</h2>
-                    <h3 class="text-gray-500 mb-3">Marketing</h3>
+                    <h3 class="text-slate-500 mb-3">Marketing</h3>
                     <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                     <span class="inline-flex">
-                      <a class="text-gray-700">
+                      <a class="text-slate-700">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" class="w-5 h-5" viewBox="0 0 24 24">
                           <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                         </svg>
                       </a>
-                      <a class="ml-2 text-gray-700">
+                      <a class="ml-2 text-slate-700">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" class="w-5 h-5" viewBox="0 0 24 24">
                           <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                         </svg>
                       </a>
-                      <a class="ml-2 text-gray-700">
+                      <a class="ml-2 text-slate-700">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" class="w-5 h-5" viewBox="0 0 24 24">
                           <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                         </svg>

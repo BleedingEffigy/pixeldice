09a9ec1f9e63f675cd0263a100f1fc8fbc87ed4b
Alex Hernandez <ahern077@gmail.com>
2022-01-22 08:41:08 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -90,7 +90,7 @@ export default function Home() {
                   <svg xmlns="http://www.w3.org/2000/svg" class="text-red-500 w-12 h-12 mb-3 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z" />
                   </svg>
-                  <h2 class="title-font font-medium text-3xl text-white">128</h2>
+                  <h2 class="title-font font-medium text-3xl text-white">123</h2>
                   <p class="leading-relaxed">Total Specifications</p>
                 </div>
               </div>
